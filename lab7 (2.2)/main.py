8dcca298e56801d2b56a1e6160f0e85fc22a04ac
Slava <Slava.lokshin@mail.ru>
2024-04-04 23:41:25 +0300

    Добавил поддержку арифметических операций

--- lab7 (2.2)/main.py
+++ lab7 (2.2)/main.py
@@ -6,8 +6,28 @@ from pprint import pprint
 
 
 @dataclass
+class TypeSpecifier(abc.ABC):
+    pass
+
+
+class SimpleType(enum.Enum):
+    Char = "CHAR"
+    Short = "SHORT"
+    Int = "INT"
+    Long = "LONG"
+    Float = "FLOAT"
+    Double = "DOUBLE"
+    Signed = "SIGNED"
+    Unsigned = "UNSIGNED"
+
+
+class Expression(abc.ABC):
+    pass
+
+
+@dataclass
 class ConstantExpression:
-    body: str
+    expression: Expression
 
 
 @dataclass
@@ -19,11 +39,6 @@ class Enumerator:
 @dataclass
 class EnumeratorList:
     body: list[Enumerator]
-
-
-@dataclass
-class TypeSpecifier(abc.ABC):
-    pass
 
 
 @dataclass
@@ -48,15 +63,27 @@ class EmptyEnumStatement(EnumStatement):
     identifier: str
 
 
-class SimpleType(enum.Enum):
-    Char = "CHAR"
-    Short = "SHORT"
-    Int = "INT"
-    Long = "LONG"
-    Float = "FLOAT"
-    Double = "DOUBLE"
-    Signed = "SIGNED"
-    Unsigned = "UNSIGNED"
+@dataclass
+class IdentifierExpression(Expression):
+    identifier: str
+
+
+@dataclass
+class IntExpression(Expression):
+    value: int
+
+
+@dataclass
+class BinaryOperationExpression(Expression):
+    left: Expression
+    operation: str
+    right: Expression
+
+
+@dataclass
+class UnaryOperationExpression(Expression):
+    operation: str
+    expression: Expression
 
 
 @dataclass
@@ -128,6 +155,16 @@ NCommaOpt = pe.NonTerminal('CommaOpt')
 NPointerOpt = pe.NonTerminal('PointerOpt')
 
 
+NExpression = pe.NonTerminal('Expression')
+
+NArithmeticExpression = pe.NonTerminal('ArithmeticExpression')
+NTerm = pe.NonTerminal('Term')
+NAddOperation = pe.NonTerminal('AddOperation')
+
+NFactor = pe.NonTerminal('Factor')
+NMultyOperation = pe.NonTerminal('MultyOperation')
+
+
 def make_keyword(image):
     return pe.Terminal(image, image, lambda _: None, priority=10)
 
@@ -137,7 +174,9 @@ KW_ENUM = make_keyword('enum')
 KW_CHAR, KW_SHORT, KW_INT, KW_LONG, KW_FLOAT, KW_DOUBLE, KW_SIGNED, KW_UNSIGNED = \
     map(make_keyword, 'char short int long float double signed unsigned'.split())
 
-KW_IDENTIFIER = pe.Terminal('IDENTIFIER', r'[A-Za-z_]([A-Za-z_0-9])*', str)
+INTEGER = pe.Terminal('IDENTIFIER', r'[0-9]*', str)
+
+IDENTIFIER = pe.Terminal('IDENTIFIER', r'[A-Za-z_]([A-Za-z_0-9])*', str)
 
 KW_POINTER = pe.Terminal('POINTER', r'(\*)*', str)
 
@@ -155,7 +194,7 @@ NAbstractDeclaratorsOpt |= NAbstractDeclarators
 NAbstractDeclarators |= NAbstractDeclarator, lambda a: [a]
 NAbstractDeclarators |= NAbstractDeclarators, ',', NAbstractDeclarator, lambda ads, a: ads + [a]
 
-NAbstractDeclarator |= NPointerOpt, KW_IDENTIFIER, NListArraysOpt, AbstractDeclarator
+NAbstractDeclarator |= NPointerOpt, IDENTIFIER, NListArraysOpt, AbstractDeclarator
 
 NPointerOpt |= KW_POINTER
 NPointerOpt |= lambda: ""
@@ -163,7 +202,7 @@ NPointerOpt |= lambda: ""
 
 NListArraysOpt |= lambda: []
 NListArraysOpt |= '[', NModifySimpleType, ']', NListArraysOpt, lambda mst, lao: lao + [mst]
-NModifySimpleType |= KW_IDENTIFIER
+NModifySimpleType |= IDENTIFIER
 NModifySimpleType |= NSimpleType, lambda st: str(st)
 
 
@@ -189,26 +228,50 @@ NEnumStatement |= NFullEnumStatement
 NFullEnumStatement |= NIdentifierOpt, '{', NEnumeratorList, NCommaOpt, '}', FullEnumStatement
 
 NIdentifierOpt |= lambda: ""
-NIdentifierOpt |= KW_IDENTIFIER
+NIdentifierOpt |= IDENTIFIER
 
 NEnumStatement |= NEmptyEnumStatement, EmptyEnumStatement
 
-NEmptyEnumStatement |= KW_IDENTIFIER
+NEmptyEnumStatement |= IDENTIFIER
 
 NEnumeratorList |= NEnumerator, lambda e: [e]
 NEnumeratorList |= NEnumeratorList, ',', NEnumerator, lambda el, e: el + [e]
 
-NEnumerator |= KW_IDENTIFIER, NEnumeratorExpressionOpt, Enumerator
+NEnumerator |= IDENTIFIER, NEnumeratorExpressionOpt, Enumerator
 
-NEnumeratorExpressionOpt |= '=', NConstantExpression
+NEnumeratorExpressionOpt |= '=', NConstantExpression, ConstantExpression
 NEnumeratorExpressionOpt |= lambda: ""
-
-# TODO: потом тут будет вычисление выражения
-NConstantExpression |= KW_IDENTIFIER
 
 NCommaOpt |= ',', lambda: True
 NCommaOpt |= lambda: False
 
+
+NConstantExpression |= NExpression
+
+NExpression |= NArithmeticExpression
+
+NArithmeticExpression |= NTerm
+NArithmeticExpression |= '+', NTerm, lambda t: UnaryOperationExpression('+', t)
+NArithmeticExpression |= '-', NTerm, lambda t: UnaryOperationExpression('-', t)
+NArithmeticExpression |= NArithmeticExpression, NAddOperation, NTerm, BinaryOperationExpression
+
+NAddOperation |= '+', lambda: '+'
+NAddOperation |= '-', lambda: '-'
+
+NTerm |= NFactor
+NTerm |= NTerm, NMultyOperation, NFactor, BinaryOperationExpression
+
+NMultyOperation |= '*', lambda: '*'
+NMultyOperation |= '/', lambda: '/'
+
+NFactor |= '(', NExpression, ')'
+
+NFactor |= INTEGER, IntExpression
+
+NFactor |= IDENTIFIER, IdentifierExpression
+
+
+#  TODO: Добавить вычисление sizeof ( ??? )
 
 def main():
     p = pe.Parser(NProgram)
