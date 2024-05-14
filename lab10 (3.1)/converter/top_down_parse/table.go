d759f6599dd0d4c542826c56337b0cfc54978194
Slava <Slava.lokshin@mail.ru>
2024-05-15 00:44:26 +0300

    Убрал warning

--- lab10 (3.1)/converter/top_down_parse/table.go
+++ lab10 (3.1)/converter/top_down_parse/table.go
@@ -11,12 +11,6 @@ const (
 	Rewriting     = "REWRITING"
 	RewritingOpt  = "REWRITING_OPT"
 	Body          = "BODY"
-	nonTerminal   = "NonTerminal"
-	terminal      = "Terminal"
-	axiomSign     = "AxiomSign"
-	OpenBracket   = "OpenBracket"
-	CloseBracket  = "CloseBracket"
-	eof           = "Eof"
 )
 
 func newTableKey(nonTerminal, terminal string) string {
@@ -24,31 +18,9 @@ func newTableKey(nonTerminal, terminal string) string {
 }
 
 func terminalValue() []string {
-	return []string{Declarations, RewritingRule, Rewriting, RewritingOpt, Body}
+	return []string{"DECLARATIONS", "REWRITING_RULE", "REWRITING", "REWRITING_OPT", "BODY"}
 }
 
 func isTerminal(s string) bool {
 	return !slices.Contains(terminalValue(), s)
-}
-
-func newTable() map[string][]string {
-	return map[string][]string{
-		newTableKey(Declarations, axiomSign):   {RewritingRule, Declarations},
-		newTableKey(Declarations, nonTerminal): {RewritingRule, Declarations},
-		newTableKey(Declarations, eof):         {},
-
-		newTableKey(RewritingRule, axiomSign):   {axiomSign, nonTerminal, Rewriting},
-		newTableKey(RewritingRule, nonTerminal): {nonTerminal, Rewriting},
-
-		newTableKey(Rewriting, OpenBracket): {OpenBracket, Body, CloseBracket, RewritingOpt},
-
-		newTableKey(RewritingOpt, axiomSign):   {},
-		newTableKey(RewritingOpt, nonTerminal): {},
-		newTableKey(RewritingOpt, OpenBracket): {OpenBracket, Body, CloseBracket, RewritingOpt},
-		newTableKey(RewritingOpt, eof):         {},
-
-		newTableKey(Body, nonTerminal):  {nonTerminal, Body},
-		newTableKey(Body, terminal):     {terminal, Body},
-		newTableKey(Body, CloseBracket): {},
-	}
 }
